83c279862a51dd787be4eedbd636636759d8139d
Zalak Shah <zsshah@iu.edu>
2018-01-15 03:12:30 +0000

    Deleting old 1.69IGTFNEW and 1.69NEW
    git-svn-id: https://vdt.cs.wisc.edu/svn/certs/trunk@24149 4e558342-562e-0410-864c-e07659590f8c

--- cadist/CA-Certificates-Base/mk-index.pl
+++ cadist/CA-Certificates-Base/mk-index.pl
@@ -192,6 +192,10 @@ print OH "The ca-certs-version file used by pacman is \n";
 print OH "<a href='ca-certs-version'>here</a>. \n";
 #[12/27/17]print OH "A list of md5sums is available in \n";
 #[12/27/17]print OH "<a href='cacerts_md5sum.txt'>cacerts_md5sum.txt file</a>.\n";
+#[01/12/18]Uncommenting the above comments
+print OH "A list of md5sums is available in \n";
+print OH "<a href='cacerts_md5sum.txt'>cacerts_md5sum.txt file</a>.\n";
+
 print OH "A list of sha256sums is available in \n";
 print OH "<a href='cacerts_sha256sum.txt'>cacerts_sha256sum.txt file</a>.\n";
 print OH "<table border='1' cellspacing='2' cellpadding='2'><tr>";
