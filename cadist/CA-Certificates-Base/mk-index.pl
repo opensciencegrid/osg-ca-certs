be8e1e56d81f0300e9e3eb96d1f0e97692c0fef9
Zalak Shah <zsshah@iu.edu>
2018-11-06 20:45:03 +0000

    Updated to IGTF version 1.94, OSG version 1.77IGTFNEW
    git-svn-id: https://vdt.cs.wisc.edu/svn/certs/trunk@24693 4e558342-562e-0410-864c-e07659590f8c

--- cadist/CA-Certificates-Base/mk-index.pl
+++ cadist/CA-Certificates-Base/mk-index.pl
@@ -191,8 +191,9 @@ print OH "<a href='CHANGES'>CA certificate change log</a>. \n";
 print OH "The ca-certs-version file used by pacman is \n";
 print OH "<a href='ca-certs-version'>here</a>. \n";
 #[10/30/18] commenting out to remove MD5 sum
-#print OH "A list of md5sums is available in \n";
-#print OH "<a href='cacerts_md5sum.txt'>cacerts_md5sum.txt file</a>.\n";
+#[11/06/18] uncommenting the following code to again include MD5 sum
+print OH "A list of md5sums is available in \n";
+print OH "<a href='cacerts_md5sum.txt'>cacerts_md5sum.txt file</a>.\n";
 
 print OH "A list of sha256sums is available in \n";
 print OH "<a href='cacerts_sha256sum.txt'>cacerts_sha256sum.txt file</a>.\n";
